6439bb5559f7a312b42a02f763a04b60bf0ea88b
SamuelMeline <melsam0905@gmail.com>
2022-05-17 10:54:29 +0200

    Alignement des cartes + logo

--- paypal.js
+++ paypal.js
@@ -1,1 +1,16 @@
-paypal.Buttons().render('#paypal-button-container');
+paypal.Buttons({
+  createOrder: function(data, actions) {
+    return actions.order.create({
+      purchase_units: [{
+        amount: {
+          value: '0.10'
+        }
+      }]
+    });
+  },
+  onApprove: function(data, actions) {
+    return actions.order.capture().then(function(details) {
+      alert('Transaction completed by ' + details.payer.name.given_name)
+    });
+  }
+}).render('#paypal-button-container');
